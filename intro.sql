5c2bd5da68aad30aef43a87043cff64b6cc156c6
nataschaberg <natalja.wittenberg@gmx.de>
2022-11-05 14:23:09 +0100

    intro lab

---
+++ intro.sql
@@ -0,0 +1,17 @@
+use sakila;
+
+select * from actor;
+select * from film;
+select * from customer;
+
+select title from film;
+
+select name as language from language;
+
+select * from store;
+-- two store id's thus two stores in total
+
+select * from staff;
+-- two employees listed under staff table
+
+select first_name from staff;
